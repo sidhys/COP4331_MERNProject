ef498f289254ea8b9fab1b77be27d9f0b67fcd7b
LincolnHuber <162394852+LincolnHuber@users.noreply.github.com>
2026-04-02 23:38:17 -0400

    Update routes.tsx

--- Front-End/src/app/routes.tsx
+++ Front-End/src/app/routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router";
-import { Root } from "./components/ui/Root.tsx";
-import { Store } from "./components/ui/Store.tsx";
-import { SignUp } from "./components/ui/SignUp.tsx";
-import { VerifyEmail } from "./components/ui/VerifyEmail.tsx";
-import { Login } from "./components/ui/Login";
+import { Root } from "./components/ui/root.tsx";
+import { Store } from "./components/ui/store.tsx";
+import { SignUp } from "./components/ui/signUp.tsx";
+import { VerifyEmail } from "./components/ui/verifyEmail.tsx";
+import { Login } from "./components/ui/login";
 
 export const router = createBrowserRouter([
   {
